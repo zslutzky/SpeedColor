7992c0bef76efcb7db25e1d3d0eeba6e71a759b2
zslutzky <zslutzky430@gmail.com>
2015-01-29 13:35:22 -0600

    Create function.js

---
+++ function.js
@@ -0,0 +1,35 @@
+$(function() {
+    
+    var $speedometer = $('#speedometer');
+    var currSpeed = 0;
+    
+    $('.paragraph').cursometer({
+        onUpdateSpeed: function(speed) {
+            currSpeed = speed;
+        },
+        updateSpeedRate: 20
+    });
+
+    $('.paragraph').mouseover(function(){
+
+        if(currSpeed > 0) {
+         $('.paragraph').css({'background-color': '#ccc','transition': 'background-color 1s'})
+        
+	if(currSpeed > 1) {
+	    $('.paragraph').css({'background-color': 'lightblue','transition': 'background-color 1s'})
+        
+	if (currSpeed > 2) {
+		 $('.paragraph').css({'background-color': 'blue','transition': 'background-color 1s'})
+        
+	if(currSpeed > 3) {
+		     $('.paragraph').css({'background-color': 'darkblue','transition': 'background-color 1s'})
+		     }
+		 }
+	    }  	 else $('.paragraph').css({'background-color': 'white','transition': 'background-color 1s'})
+}
+	 else $('.paragraph').css({'background-color': 'white','transition': 'background-color 1s'})
+
+	     }
+    );
+
+});
